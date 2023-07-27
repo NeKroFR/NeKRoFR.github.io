ea7866e16af02aceee73d11dd5182c3ba2327470
Nekro <70316327+NeKroFR@users.noreply.github.com>
2023-07-27 14:09:35 +0200

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,3 @@
+npm install -D tailwindcss
+git clone https://github.com/NeKroFR/NeKroFR.github.io.git
+mv NeKroFR.github.io site
---
+++ start.sh
@@ -0,0 +1,2 @@
+#!bin/sh
+npx tailwindcss -i ./style.css -o ./site/style.css --watch
