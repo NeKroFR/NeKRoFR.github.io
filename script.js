a9a289e8956433be7f0e80552803a010e6bfbef7
nk0 <nk0@nk0.nk0>
2024-12-05 16:47:35 +0100

    submit ctf

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ window.onload = function() {
         fontFamily: 'Ubuntu Mono, Monaco, Courier, monospace',
         forceFocus: false,
         textSize: '1em',
-        promptPS: '$ ',
+        promptPS: '❯ ',
         width: '100%',
         height: '100vh',
         margin: '0',
@@ -22,11 +22,24 @@ window.onload = function() {
         padding: '10px',
     });
 
+    const bufferSize = 0x1000;
+
     (async function() {
         await shell.printHTML('<span style="color: #fff; font-size: 2em;">Nothing to see here.</span>');
         while (true) {
-            await shell.input('');
-            await shell.type(`You are in a sandbox.`, 50);
+            const input = await shell.input("");
+            const truncatedInput = input.slice(0, bufferSize);
+            if (input.length > 2*bufferSize) {
+                await shell.printHTML('<span style="color: #fff;">[1]    12573 segmentation fault (core dumped)  ./qemu-x86_64 -hda disk.img -monitor stdio</span>');
+                await new Promise(() => {});
+            }
+            if (input.length > bufferSize) {
+                await shell.print('Buffer overflow detected! What are you trying to do?');
+            }
+            else {
+                await shell.type("You are in a sandbox. You can't do anything here.");
+            }
         }
-    })();
+        // 000124231556123420756f59206e61632065657372756f796e776f206579652074202c7320796568726568776f6e20656e6968746f74206765657320726568202f3a206500115512231210015
+    })();    
 };
